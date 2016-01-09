02665479cfe49dd93ae014189fafdfc734f5144f
Md. Mozahidur Rahman <mr.rousnay@gmail.com>
2016-01-09 17:20:34 +0600

    Single post layout modified

--- single.php
+++ single.php
@@ -9,26 +9,54 @@
 
 get_header(); ?>
 
+<?php 
+//get the blog info
+$blog_url = get_permalink( get_option('page_for_posts' ) );
+$blog_title = get_the_title( get_option('page_for_posts' ) );
+?>
+	<div class="title-wrapper">
+		<div class="container">
+			<div class="row">
+			<div class="col-xs-12">
+				<h1 class="entry-title"><?php echo $blog_title;?></h1> | <a href="<?php echo $blog_url;?>" class="back-link">back</a>
+			</div>
+			</div>
+		</div>
+	</div>
 	<div id="primary" class="container content-area">
 		<div class="row">
-			<main id="main" class="site-main" role="main">
+			<div class="col-md-9 blog-listing">
+				<main id="main" class="site-main" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+				<?php
+				while ( have_posts() ) : the_post();
 
+					$thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
+					$url = $thumb[0];
+				?> 
+
+				<div class="post-img">
+				<img class="img-responsive" src="<?php echo $url; ?>">
+				</div>
+				
+				<?php
 				get_template_part( 'template-parts/content', get_post_format() );
 
-				the_post_navigation();
+					//the_post_navigation();
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+					// If comments are open or we have at least one comment, load up the comment template.
+					// if ( comments_open() || get_comments_number() ) :
+					// 	comments_template();
+					// endif;
 
-			endwhile; // End of the loop.
-			?>
+				endwhile; // End of the loop.
+				?>
 
-			</main><!-- #main -->
+				</main><!-- #main -->
+			</div>
+		<div class="col-md-3 sidebar" role="complementary">
+			<?php dynamic_sidebar( 'blog_widgets' ); ?>
+		</div>
 		</div><!-- .row -->
 	</div><!-- #primary -->
 
